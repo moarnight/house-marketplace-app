ccc7cabf920d355e3bff100aa4bc9df80614384a
moarnight <moarnight@gmail.com>
2022-11-04 21:24:45 +0100

    Add starter files

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCVASeQy2UFokrL1Yxks1rY6IB3yXm1MRA',
+  authDomain: 'house-marketplace-app-18e50.firebaseapp.com',
+  projectId: 'house-marketplace-app-18e50',
+  storageBucket: 'house-marketplace-app-18e50.appspot.com',
+  messagingSenderId: '328790701290',
+  appId: '1:328790701290:web:1f28ff1f34d8c205bad393',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
