5970edc7d13ba5110ced7d0776feda24581f7c79
moarnight <moarnight@gmail.com>
2022-11-11 20:03:18 +0100

    Add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <>
-      <h1>House Marketplace App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/sign-in" element={<Signin />}></Route>
+          <Route path="/sign-up" element={<Signup />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
